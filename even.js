f0a5d7fbc74b3cc36ad3a5febf426f27a36fdf7a
Taklid <mdtaklid02@.com>
2024-09-02 15:50:27 +0600

    All-practice-js

--- even.js
+++ even.js
@@ -5,3 +5,45 @@ while(num <=20){
     if(num % 2 === 1)
         console.log('even number', num);
 }
+
+
+// for(let num = 0; num <= 20; num++){
+//     console.log('list:', num);
+// }
+
+
+for(let i = 0; i <= 20; i=i+2){
+    console.log('list:', i);
+}
+
+for(let i = 0; i <= 20; i=i+5){
+    console.log('list:', i);
+}
+
+ let sum = 0;
+
+for(let i = 10; i<=30; i++){
+    
+    sum =sum + i;
+    console.log(i);
+    
+}
+console.log('sum number', sum);
+
+        // decremental for loop
+for(let i = 10; i >=0; i--){
+    console.log(i);
+}
+
+
+// for(let i = 1; i <= 30; i++){
+
+//     if(i % 3 === 0 || i % 5 === 0 )
+//         console.log('list:', i);
+// }
+
+for(let i = 1; i <= 30; i++){
+
+    if(i % 3 === 0 && i % 4 === 0 )
+        console.log('list:', i);
+}
